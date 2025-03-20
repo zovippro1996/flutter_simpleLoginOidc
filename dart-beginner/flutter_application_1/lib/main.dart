174b4572ba1213527e6dc431856e507dcde36d93
Phạm Lê Trung <plt@netcompany.com>
2025-03-20 22:42:09 +0700

    update: more study

--- dart-beginner/flutter_application_1/lib/main.dart
+++ dart-beginner/flutter_application_1/lib/main.dart
@@ -3,7 +3,81 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObjs = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg',
+  };
+
+  if (year >= 2001) {
+    print('21st century');
+  } else if (year >= 1901) {
+    print('20th century');
+  }
+
+  for (var obj in flybyObjs) {
+    print(obj);
+  }
+
+  for (var i = 1; i <= 12; i++) {
+    print(i);
+  }
+
+  while (year < 2016) {
+    year++;
+  }
+
+  print(year);
+
+  var result = fibonacci(20);
+  print(result);
+
+  flybyObjs.where((name) => name.contains('turn')).forEach(print);
+
+  var voyage = Spacecraft('Voyage I', DateTime(1977, 9, 5));
+  voyage.describe();
+
+  var voyager3 = Spacecraft.unlaunched('Voyager III');
+
+  voyager3.describe();
+
+  print('Hello, World!');
+  // runApp(const MainApp());
+}
+
+int fibonacci(int n) {
+  if (n == 0 || n == 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+class Spacecraft {
+  String name;
+  DateTime? launchDate;
+
+  // Read-only non-final property
+  int? get launchYear => launchDate?.year;
+
+  // Constructor
+  Spacecraft(this.name, this.launchDate) {}
+
+  // Named constructored
+  Spacecraft.unlaunched(String name) : this(name, null);
+
+  // Method.
+  void describe() {
+    print('Spacecraft: $name');
+    // Type promotion does not work on getter.
+    var launchDate = this.launchDate;
+    if (launchDate != null) {
+      int years = DateTime.now().difference(launchDate).inDays ~/ 365;
+      print('Launched: $launchYear ($years years ago)');
+    } else {
+      print('Unlaunched');
+    }
+  }
 }
 
 class MainApp extends StatelessWidget {
